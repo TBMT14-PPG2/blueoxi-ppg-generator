e3d9791e65c9e5de763369de9dab4a568e89e0ae
PatrikSundin <patsu326@student.liu.se>
2015-09-21 20:22:57 +0200

    Create ppggenerator.m
    PPG generator for matlab

---
+++ ppggenerator.m
@@ -0,0 +1,41 @@
+clear all
+
+noise=0.5;
+DC=15;
+AC=1;
+DN=1;
+fluctuation=1;
+samplingrate=600;
+heartrate=100;
+time=20;
+
+t=0:1/samplingrate:time;
+a=4*sin(t*2*pi)+4;
+b=sin(t*2*pi+pi*1.4)*4-3.5;
+
+
+for i=1:length(a)
+    if a(i)<0
+        a(i)=0;
+    end
+end
+
+for i=1:length(b)
+    if b(i)<0
+        b(i)=0;
+    end
+end
+    
+c=AC*(a+DN*b*4);
+%---------Add noise
+
+c=c+noise*rand(1,length(c));
+
+%---------Add DC
+
+c=c+DC;
+%--------Add fluctuation
+
+F=0.05*sin(t/2)+1;
+c=fluctuation*c.*F;
+plot(t,c)
