2d5868770357e5fe2547e23c16a402dfccb09605
PatrikSundin <patsu326@student.liu.se>
2015-09-23 14:44:37 +0200

    Added some comments

--- ppggenerator.m
+++ ppggenerator.m
@@ -1,13 +1,13 @@
 clear all
 
-noise=0.5;
-DC=15;
-AC=1;
-DN=1;
-fluctuation=1;
+noise=1;    %Noise amplitude
+DC=15;      %DC amplitude
+AC=1;       %AC amplitude
+DN=1;       %Dicrotic notch size
+fluctuation=1;  %Fluctiation amplitude
 samplingrate=60;
 heartrate=60;
-time=10;
+time=10;    %Seconds
 
 t=0:1/samplingrate:time;
 a=4*sin(t*2*pi*heartrate/60)+4;
@@ -29,7 +29,7 @@ end
 c=AC*(a+DN*b*4);
 %---------Add noise
 
-c=c+noise*rand(1,length(c));
+c=c+noise*rand(1,length(c))/2;
 
 %---------Add DC
 
@@ -38,4 +38,6 @@ c=c+DC;
 
 F=0.05*sin(t/2)+1;
 c=fluctuation*c.*F;
+
+Generated_PPG_signal=c;     %Final generated PPG signal
 plot(t,c)
